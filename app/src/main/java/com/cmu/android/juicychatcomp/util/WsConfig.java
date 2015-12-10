62260c324bb409edc33a94d8d8d86bc8ab2d52fe
M4jOr42z <zxq24@case.edu>
2015-12-10 15:10:48 -0800

    change server to WebMobileGroupchatServer

--- app/src/main/java/com/cmu/android/juicychatcomp/util/WsConfig.java
+++ app/src/main/java/com/cmu/android/juicychatcomp/util/WsConfig.java
@@ -1,5 +1,5 @@
 package com.cmu.android.juicychatcomp.util;
 
 public class WsConfig {
-	public static final String URL_WEBSOCKET = "ws://192.168.0.15:8080/juicyChatServer/juicyChat?";
+	public static final String URL_WEBSOCKET = "ws://172.29.92.43:8080/WebMobileGroupChatServer/chat?";
 }
